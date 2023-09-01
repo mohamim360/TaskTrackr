73bb155992098b0f97bb8a641163b30bd2622b92
mohamim360 <mohamim360@gmail.com>
2023-09-01 07:41:49 +0600

    changes in DB.js

--- utilities/database.js
+++ utilities/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: "mysql",
-  host: "localhost",
+  host: process.env.DB_HOST,
 });
 
 module.exports = sequelize;
