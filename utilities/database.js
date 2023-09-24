72f8cfb49c405f9e6c1746b528b4511ab2c74b84
mohamim360 <mohamim360@gmail.com>
2023-09-24 13:11:37 +0600

    dialectOptions

--- utilities/database.js
+++ utilities/database.js
@@ -4,6 +4,9 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: "mysql",
   host: process.env.DB_HOST,
+  dialectOptions: {
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+  }
 });
 
 module.exports = sequelize;
