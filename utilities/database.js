66045cb10c86990e66d12d38b1d0d446f522c5af
mohamim360 <mohamim360@gmail.com>
2023-09-24 13:20:41 +0600

    port

--- utilities/database.js
+++ utilities/database.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: "mysql",
-  host: process.env.DB_HOST,
-  dialectOptions: {
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+    port: 3306,
   }
-});
+);
 
 module.exports = sequelize;
